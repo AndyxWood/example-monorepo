15edcb1fc9246e6f8c9d7f5e2164d1739350ac00
Andy Wood <andy@auditsight.com>
2022-08-09 16:32:24 -0500

    Resolved git submodule, contents added

---
+++ looking-glass/frontend/next.config.js
@@ -0,0 +1,9 @@
+const withTM = require('next-transpile-modules')(['common'])
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = withTM(nextConfig);
